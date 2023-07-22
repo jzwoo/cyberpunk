c90919caa640b195bd92b1b526b3c00d7f34ea90
jzwoo <jzwoo98@gmail.com>
2023-07-22 11:53:21 +0800

    Style and make nav bar functional

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,25 +1,28 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 import "./NavBar.css";
 
 const NavBar: React.FC = () => {
     return (
-        <div className="navBar">
-            <div>
-                HOME
-            </div>
-
-            <div>
-                STORE
-            </div>
-
-            <div>
-                CART
-            </div>
-
-            <div>
-                LOGIN
-            </div>
-        </div>
+        <nav className="navBar">
+            <ul>
+                <li>
+                    <NavLink to="/" className={({isActive}) => isActive ? "active" : ""}>
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/store" className={({isActive}) => isActive ? "active" : ""}>
+                        Store
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/login" className={({isActive}) => isActive ? "active" : ""}>
+                        Login
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
